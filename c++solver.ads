5316022abb23cc5d292c3f6b484bd343ae9e16f8
linux-terminal <43521603+linux-terminal@users.noreply.github.com>
2018-11-16 16:48:00 +0800

    Update c++solver.ads

--- c++solver.ads
+++ c++solver.ads
@@ -1,8 +1,8 @@
 #include <iostream>
+using namespace std;
 int main()
 {
     float a,b,c,delta,x1,x2;
-    a=b=c=delta=x1=x20;
     cout << "ax^2+bx+c=0\n";
     cout << "a=";
     cin >> a;
@@ -10,7 +10,7 @@ int main()
     cin >> b;
     cout << "c=";
     cin >> c;
-    delta=b^2-4*a*c;
+    delta=b*b-4*a*c;
     x1=(-b+delta)/(2*a);
     x2=(-b-delta)/(2*a);
     cout << "x1=" << x1 << "\n" << "x2=" << x2 <<endl;
